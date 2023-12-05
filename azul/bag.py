083afa3f6c49651c8d65fdd496624f34129aff81
Gabriela Michalkova <m.demetrov@gmail.com>
2023-12-05 16:51:46 +0100

    bag v.1

--- azul/bag.py
+++ azul/bag.py
@@ -2,15 +2,34 @@ from __future__ import annotations
 from typing import List
 from simple_types import Tile, RED, BLUE, YELLOW, GREEN, BLACK, compress_tile_list
 from usedTiles import usedTiles
+import random
 
 class Bag:
-    self.used_tiles: usedTiles
+    used_tiles: usedTiles
+    tiles: List[Tile]
     def __init__(self, used_tiles: usedTiles) -> None:
         self.used_tiles = used_tiles
+        self.tiles = [RED, BLUE, YELLOW, GREEN, BLACK]*20
         pass
 
     def state(self) -> str:
-        pass
+        if (not self.tiles):
+            return "The bag is empty"
+        state: str = compress_tile_list(self.tiles) + " -> "
+        for tileType in [RED, BLUE, YELLOW, GREEN, BLACK]:
+            state += str(tileType) + ":" + str(self.tiles.count(tileType)) + " "
+        return state
 
-    def take(self, count: int) -> list[Tile]:
-        pass
+    def take(self, count: int, testing: bool = False) -> List[Tile]:
+        taken: List[Tile] = list()
+        for i in range(count):
+            if(not self.tiles):
+                self.used_tiles.takeAll()
+                if(not self.tiles):
+                    raise IndexError("Insuficient number of tiles")
+            if(testing):#pseudorandomForTesting (hopefully)
+                idxOfTile = 0
+            else:
+                idxOfTile = random.randrange(0,len(self.tiles))
+            taken.append(self.tiles.pop(idxOfTile))
+        return taken
